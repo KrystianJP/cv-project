b119c80da2e8263e1f538a6e7f216e5afcbef45e
Krystian JP <krystianjp97@gmail.com>
2022-05-05 17:05:53 +0100

    initial component setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Form from "./components/Form";
 
 class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <header>CV Project</header>
+        <Form />
+      </div>
+    );
   }
 }
 
