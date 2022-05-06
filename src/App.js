7aec8ba5d0c94206e2bbe3f6e63ba3293b5a1ac9
Krystian JP <krystianjp97@gmail.com>
2022-05-06 20:33:02 +0100

    added img, text updates with name styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./styles/style.css";
 import Cv from "./components/Cv";
 
 class App extends React.Component {
+  #classes = {
+    name: "name",
+  };
+
   constructor(props) {
     super(props);
 
@@ -31,6 +35,8 @@ class App extends React.Component {
     for (let key in this.state) {
       if (this.state.hasOwnProperty(key)) {
         let cvClassName = "cv-text";
+        cvClassName += " cv-" + this.#classes[key];
+
         result.push(
           <li key={key} className={cvClassName}>
             {this.state[key]}
@@ -38,7 +44,24 @@ class App extends React.Component {
         );
       }
     }
+    result = result
+      .slice(0, 1)
+      .concat(this.getUserImage())
+      .concat(result.slice(1));
     return result;
+  }
+
+  getUserImage() {
+    let image = (
+      <img
+        key="image"
+        src="https://imgs.search.brave.com/wVaCsBwZBwBTc89EXf7S-Ef3xr4UQFVcxhdf1QEkd6U/rs:fit:916:980:1/g:ce/aHR0cDovL2Nkbi5v/bmxpbmV3ZWJmb250/cy5jb20vc3ZnL2lt/Z18yMTAzMTgucG5n"
+        className="user-img"
+        alt="user"
+      ></img>
+    );
+
+    return image;
   }
 
   render() {
