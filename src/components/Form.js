d636d461c36a92c54c7d412d8f63ee5cb5598f3d
Krys JP <krysjp97@outlook.com>
2022-05-14 15:08:17 +0100

    converted Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,16 @@ import EducationInfo from "./EducationInfo";
 import WorkInfo from "./WorkExperience";
 import "../styles/style.css";
 
-class Form extends React.Component {
-  render() {
-    return (
-      <div className="form-container">
-        <div className="form">
-          <GeneralInfo changeValues={this.props.changeValues} />
-          <EducationInfo changeValues={this.props.changeValues} />
-          <WorkInfo changeValues={this.props.changeValues} />
-        </div>
+const Form = (props) => {
+  return (
+    <div className="form-container">
+      <div className="form">
+        <GeneralInfo changeValues={props.changeValues} />
+        <EducationInfo changeValues={props.changeValues} />
+        <WorkInfo changeValues={props.changeValues} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Form;
